e6775691ef2e08fbab3662d41d8a2c8039fed4b7
navyjia <arenas@aliyun.com>
2023-12-08 19:58:16 +0800

    perf deploy

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
   build: {
-    outDir: "docs"
+    outDir: "dist"
   },
   plugins: [
     vue(),
